6c24344315c9be70b1640c2d5403dc7e34f2b0ac
JordanaJ12 <jordana.jesus.2001@gmail.com>
2021-12-28 20:29:39 -0300

    Primeiro Commit no IntelliJ

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a= 9;
+        int b= 11;
+        System.out.println("Olá Mundo! Prazer, Jordana. Tenho " + (a+b) + " anos.");
+         */
+    }
+}

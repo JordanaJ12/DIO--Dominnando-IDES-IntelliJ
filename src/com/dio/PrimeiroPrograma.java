38231b577f87acf3e3c4a16cac4b861b47631968
JordanaJ12 <jordana.jesus.2001@gmail.com>
2021-12-28 20:48:11 -0300

    Criando Class Livro

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,47 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O labirito dos Ossos",  280);
+
+        System.out.println(livro1);
+
         /*int a= 9;
         int b= 11;
         System.out.println("Olá Mundo! Prazer, Jordana. Tenho " + (a+b) + " anos.");
          */
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
